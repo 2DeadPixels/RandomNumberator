74169d7f656aaae554d54b9131af1de1462d7d38
Patrick <patrick.schaller@agropur.com>
2022-10-30 20:30:13 -0500

    Add project files.

---
+++ RandomNumberator/NumberatorGenerate.cs
@@ -0,0 +1,16 @@
+﻿namespace RandomNumberator
+{
+    public class NumberatorGenerate
+    {
+        public int GenerateMyNumber(int start, int end)
+        {
+            if (start == 0 && end == 0) throw new ArgumentException("Both values cannot be null");
+            if (start > end) throw new ArgumentException("Starting value cannot be higher then Ending vaule");
+
+            var randoGen = new Random();
+
+            return randoGen.Next(start, end);
+        }
+
+    }
+}
