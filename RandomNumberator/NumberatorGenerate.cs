a67a891c6a6a879efafa46d1ac98aad645053cea
Patrick <patrick.schaller@agropur.com>
2022-10-31 15:39:11 -0500

    Added some stuff

--- RandomNumberator/NumberatorGenerate.cs
+++ RandomNumberator/NumberatorGenerate.cs
@@ -4,6 +4,7 @@
     {
         public int GenerateMyNumber(int start, int end)
         {
+            // Added some stuff
             if (start == 0 && end == 0) throw new ArgumentException("Both values cannot be null");
             if (start > end) throw new ArgumentException("Starting value cannot be higher then Ending vaule");
 
